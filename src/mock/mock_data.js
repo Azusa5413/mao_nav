34727ed69f3112fd2c02904a091aa434eb28b862
Azusa5413 <azusa5413@outlook.com>
2025-11-23 15:40:14 +0800

    chore: 更新导航数据 - 2025/11/23 15:40:15

--- src/mock/mock_data.js
+++ src/mock/mock_data.js
@@ -19,6 +19,13 @@ export const mockData = {
           "url": "https://silicon-api.azusa5413.top",
           "description": "个人搭建的硅基流动API管理站",
           "icon": "https://www.faviconextractor.com/favicon/silicon-api.azusa5413.top"
+        },
+        {
+          "id": "site-1763882952085",
+          "name": "只有我知道的世界",
+          "url": "https://gal-web.hisoten.xyz/",
+          "description": "群友自建Gal答题网站",
+          "icon": "https://www.faviconextractor.com/favicon/gal-web.hisoten.xyz"
         }
       ]
     },
@@ -53,8 +60,8 @@ export const mockData = {
     },
     {
       "id": "entertainment",
-      "name": "娱乐休闲",
-      "icon": "🎮",
+      "name": "视频娱乐",
+      "icon": "📱",
       "order": 2,
       "sites": [
         {
@@ -149,6 +156,27 @@ export const mockData = {
           "url": "https://www.52pojie.cn/",
           "description": "软件安全与破解技术论坛",
           "icon": "/sitelogo/www.52pojie.cn.ico"
+        },
+        {
+          "id": "site-1763882756141",
+          "name": "NGA",
+          "url": "https://nga.cn/",
+          "description": "精英游戏论坛",
+          "icon": "https://www.faviconextractor.com/favicon/nga.cn"
+        },
+        {
+          "id": "site-1763882859776",
+          "name": "Itch Io",
+          "url": "http://itch.io",
+          "description": "独立游戏平台",
+          "icon": "https://www.faviconextractor.com/favicon/itch.io"
+        },
+        {
+          "id": "site-1763883418741",
+          "name": "初音的青葱",
+          "url": "http://xygalgame.com",
+          "description": "Galgame分享站",
+          "icon": "https://www.faviconextractor.com/favicon/xygalgame.com"
         }
       ]
     },
@@ -180,8 +208,87 @@ export const mockData = {
           "icon": "https://www.faviconextractor.com/favicon/www.namesilo.com"
         }
       ]
+    },
+    {
+      "id": "category-1763882452860",
+      "icon": "🎨",
+      "name": "绘画交流",
+      "order": 6,
+      "sites": [
+        {
+          "id": "site-1763882562152",
+          "name": "Civitai",
+          "url": "https://civitai.com/",
+          "description": "AI绘画模型站",
+          "icon": "https://www.faviconextractor.com/favicon/civitai.com"
+        },
+        {
+          "id": "site-1763882621780",
+          "name": "Hugging Face",
+          "url": "https://huggingface.co/",
+          "description": "AI绘画模型及技术分享",
+          "icon": "https://www.faviconextractor.com/favicon/huggingface.co"
+        },
+        {
+          "id": "site-1763883089999",
+          "name": "Pixiv",
+          "url": "http://www.pixiv.net",
+          "description": "绘画交流分享平台",
+          "icon": "https://www.faviconextractor.com/favicon/www.pixiv.net"
+        }
+      ]
+    },
+    {
+      "id": "category-1763883160221",
+      "icon": "💼",
+      "name": "技术学习",
+      "order": 7,
+      "sites": [
+        {
+          "id": "site-1763883218618",
+          "name": "Godot中文文档",
+          "url": "https://docs.godotengine.org/zh-cn/4.x/about/introduction.html",
+          "description": "Godot官方4.x版本中文文档",
+          "icon": "https://www.faviconextractor.com/favicon/docs.godotengine.org"
+        },
+        {
+          "id": "site-1763883339311",
+          "name": "Terraria 1.4.x 教程目录",
+          "url": "https://fs49.org/remake%e7%9a%841-4mod%e6%95%99%e7%a8%8b%e7%9b%ae%e5%bd%95/",
+          "description": "由原fs49开发者发布的泰拉瑞亚mod制作教程",
+          "icon": "https://www.faviconextractor.com/favicon/fs49.org"
+        }
+      ]
+    },
+    {
+      "id": "category-1763883450290",
+      "icon": "🎮",
+      "name": "游戏网站",
+      "order": 8,
+      "sites": [
+        {
+          "id": "site-1763883479248",
+          "name": "Terraria Wiki",
+          "url": "https://terraria.wiki.gg/zh/wiki/Terraria_Wiki",
+          "description": "Terraria官方WIKI",
+          "icon": "https://www.faviconextractor.com/favicon/terraria.wiki.gg"
+        },
+        {
+          "id": "site-1763883527461",
+          "name": "Warframe 灰机维基",
+          "url": "https://warframe.huijiwiki.com/wiki/Mainpage",
+          "description": "Warframe WIKI 中文站",
+          "icon": "https://www.faviconextractor.com/favicon/warframe.huijiwiki.com"
+        },
+        {
+          "id": "site-1763883610304",
+          "name": "Warframe Market",
+          "url": "https://warframe.market/zh-hans/",
+          "description": "Warframe非官方交易市场",
+          "icon": "https://www.faviconextractor.com/favicon/warframe.market"
+        }
+      ]
     }
   ],
-  "title": "Azusa5413的个人导航站",
-  "_fileSha": "cc28b913b18cc62503916cdea080d04a67119354"
+  "title": "Azusa5413的个人导航站"
 }
